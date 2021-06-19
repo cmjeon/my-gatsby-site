8adfec21f444abea97a8cca6f6f0d92c0e76d956
cmj <chmin82@gmail.com>
2021-06-19 23:40:23 +0900

    add deploy to s3

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,13 @@ module.exports = {
   siteMetadata: {
     title: "myGatsbySite",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "my-gatsby-site-s3",
+      },
+    },
+  ],
 };
